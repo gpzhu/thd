8d43f322fd070fb44c45a1623c1d1c096da52064
Gengping ZHU <40188354+Gengping-Zhu@users.noreply.github.com>
2021-05-09 22:50:46 -0700

    Add files via upload

---
+++ R/thd.R
@@ -0,0 +1,25 @@
+#' Thresholding model predictions
+#' @export
+#' @import raster
+#' @param sdm ENM suitability prediction
+#' @param occ Occ of long and lat
+#' @param type mtp,p10
+
+thd <- function(sdm, occ, type = ""){
+  occPredVals <- raster::extract(sdm, occ)
+  if(type == "mtp"){
+    thresh <- min(na.omit(occPredVals))
+  } else if(type == "p10"){
+    if(length(occPredVals) < 10){
+      p10 <- floor(length(occPredVals) * 0.9)
+    } else {
+      p10 <- ceiling(length(occPredVals) * 0.9)
+    }
+    thresh <- rev(sort(occPredVals))[p10]
+  }
+  sdm_thresh <- sdm
+  sdm_thresh[sdm_thresh < thresh] <- NA
+  sdm_thresh[sdm_thresh >= thresh] <- 1
+  return(sdm_thresh)
+}
+
